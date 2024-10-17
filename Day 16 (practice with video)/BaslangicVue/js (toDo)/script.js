ff59ca4927162033398ee7681f8360b678cbbf00
Akhundzadafatima <axundzadefatime.45@gmailcom>
2024-10-18 01:49:44 +0400

    just loops

--- Day 16 (practice with video)/BaslangicVue/js (toDo)/script.js
+++
@@ -1,8 +0,0 @@
-const btn=document.getElementById("btn")
-const input=document.getElementById("input")
-const mainDiv=document.getElementById("mainDiv")
-btn.addEventListener("click", ()=>{
-    const miniBox=document.createElement("li")
-    miniBox.innerHTML=input.value
-    mainDiv.append(miniBox)
-})
